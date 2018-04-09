3b57590d66797b9a4e4cedbdb462ac686e544920
Brent Harris <bdharris3@uh.edu>
2018-04-09 13:35:45 -0500

    Added a class for Salary, etc.
    Changes: 
    -- Salary class for future use as a container inside my vector.
    -- Variadic function for splitting by delimiters. Very general even though I only need it for one thing haha. Tested it and it works.
    -- Added filestream reader, tested it and it works. 
    TODO: 
    create and test a function to search through the majors to find the correct major when the client queries for it.
    Loop the accept() function on the server side so it does not close.

--- server.cpp
+++ server.cpp
@@ -14,6 +14,59 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+//#include <winsock.h>
+#include "algorithm"
+
+using namespace std;
+
+template <typename Iterator, typename Delimiter> //variadic functions made by cdhowie on stackOverflow.
+bool split_into(Iterator begin, Iterator end, Delimiter, std::string & first) //Splits by delimiter.
+{
+    first.assign(begin, end);
+    return true;
+}
+
+template <typename Iterator, typename Delimiter, typename... Strings>
+bool split_into(Iterator begin, Iterator end, Delimiter d, std::string & first, Strings & ... strings) //Splits by delimiter.
+{
+    auto i = std::find(begin, end, d);
+
+    first.assign(begin, i);
+
+    if (i != end) {
+        return split_into(++i, end, d, strings...);
+    }
+
+    return sizeof...(Strings) == 0;
+}
+class Salary //Class for containing the major, and two salaries.
+{
+    string major;
+    int lowSalary;
+    int highSalary;
+public:
+    Salary(string _major, int _lowSalary, int _highSalary)
+    {
+        major = _major;
+        lowSalary = _lowSalary;
+        highSalary = _highSalary;
+    }
+    string getMajor() const { return major; }
+    int getLowSalary() const { return lowSalary; }
+    int getHighSalary() const { return highSalary;}
+};
+
+vector<Salary> table;
+string delSpaces(string &str) //function from ddacot of stackoverflow. Removes spaces from string.
+{
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    return str;
+}
 
 
 void error(char *msg)
@@ -24,20 +77,33 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
+    ifstream input("salaries.txt");
+    string str;
+    while( getline(input, str))
+    {
+        //str = delSpaces(str);
+        string major;
+        string strLowSalary;
+        string strHighSalary;
+        split_into(str.begin(), str.end(), '\t', major, strLowSalary, strHighSalary);
+        int lowSalary = stoi(strLowSalary);
+        int highSalary = stoi(strHighSalary);
+        cout << "Major: " << major << endl;
+        cout << "Low Salary: " << lowSalary << endl;
+        cout << "High Salary: " << highSalary << endl;
+        table.push_back(Salary(major, lowSalary, highSalary));
+    }
     int sockfd, newsockfd, portno;
     unsigned int clilen;
     char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
     int n;
-    if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
-    }
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error(const_cast<char *>("ERROR opening socket"));
     bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]);
+    cout << "Please declare the port number for the server: ";
+    cin >> portno;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
